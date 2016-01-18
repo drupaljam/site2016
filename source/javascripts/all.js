63fb485d9154f8be91c095b4d59bf2fe3e6cad79
Rolf van de Krol <info@rolfvandekrol.nl>
2016-01-18 23:07:13 +0100

    First version

--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -1,1 +1,60 @@
-//= require_tree .
+
+window.get_viewport_size = function() {
+  var e = window, a = 'inner';
+  if (!('innerWidth' in window )) {
+    a = 'client';
+    e = document.documentElement || document.body;
+  }
+  return { width : e[ a+'Width' ] , height : e[ a+'Height' ] };
+};
+
+(function() {
+    var lastTime = 0;
+    var vendors = ['ms', 'moz', 'webkit', 'o'];
+    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
+        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
+        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
+                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
+    }
+ 
+    if (!window.requestAnimationFrame)
+        window.requestAnimationFrame = function(callback, element) {
+            var currTime = new Date().getTime();
+            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
+            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
+              timeToCall);
+            lastTime = currTime + timeToCall;
+            return id;
+        };
+ 
+    if (!window.cancelAnimationFrame)
+        window.cancelAnimationFrame = function(id) {
+            clearTimeout(id);
+        };
+}());
+
+(function() {
+  var orientation = undefined;
+  
+  var elements = $('.header-wrapper');
+
+  var step = function() {
+    var size = get_viewport_size();
+    var o;
+
+    if (size.height > size.width) {
+      o = 1;
+    } else {
+      o = 0;
+    }
+
+    if (orientation === undefined || orientation !== o) {
+      orientation = o;
+      elements.css('min-height', '' + size.height + 'px');
+    }
+  };
+
+  $(window).on('load resize', step);
+  step();
+})();
+
